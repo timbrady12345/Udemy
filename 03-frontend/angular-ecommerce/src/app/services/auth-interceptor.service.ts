5419cd3169b65383f7272ea847a98c4206ffd7a7
tim brady <timbrady@uri.edu>
2022-03-23 15:51:35 -0400

    fixed unimplemented auth files

--- 03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
@@ -1,35 +1,36 @@
-// import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-// import { Injectable } from '@angular/core';
-// import { OktaAuthStateService } from '@okta/okta-angular';
-// import { from, Observable } from 'rxjs';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { from, Observable } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthInterceptorService implements HttpInterceptor {
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
 
-//   constructor(private oktaAuth: OktaAuthStateService) { }
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
 
-//   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-//     return from(this.handleAccess(request, next));
-//   }
-//   private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return from(this.handleAccess(request, next));
+  }
+  private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
 
-//     //Only add an access token for secured endpoints
-//     const securedEndpoints = ['http://localhost:8080/api/orders'];
+    //Only add an access token for secured endpoints
+    const securedEndpoints = ['http://localhost:8080/api/orders'];
 
-//     if(securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+    if(securedEndpoints.some(url => request.urlWithParams.includes(url))) {
 
-//       //get access token
-//       const accessToken = await this.oktaAuth.getAccessToken();
+      //get access token
+      const accessToken = await this.oktaAuth.getAccessToken();
 
-//       //clone the request and add new header with access token
-//       request = request.clone({
-//         setHeaders: {
-//           Authorization: 'Bearer ' + accessToken;
-//         }
-//       });
-//     }
-//     return next.handle(request).toPromise();
-//   }
-// }
+      //clone the request and add new header with access token
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      });
+    }
+    return next.handle(request).toPromise();
+  }
+}
