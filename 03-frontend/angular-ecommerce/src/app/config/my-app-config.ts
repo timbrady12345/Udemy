174d4e4ffcd63b19ff8836cc94f497b9dfe4b780
tim brady <timbrady@uri.edu>
2022-03-23 21:02:35 -0400

    Something missing...

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
   oidc: {
     clientId: '0oa44wt7bl78oHpiU5d7',
     issuer: 'https://dev-7952469.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
 }
